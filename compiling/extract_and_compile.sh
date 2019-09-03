fc7752c3dcc34f45855c8400d50d90a823da1770
Melissa Kamrowski <kamro010@sentinel.morris.umn.edu>
2019-09-03 11:09:01 -0500

    Extract, uncompress, and compile NthPrime.tgz.
    Successfully pass tests.bats tests.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo hello-world
+NUM=$1
+DIR_NAME=$2
+
+# SCRATCH=$(mktemp --directory $DIR_NAME)
+
+# echo $SCRATCH
+
+tar zxf NthPrime.tgz  --directory "$DIR_NAME"
+
+cd $DIR_NAME/NthPrime
+
+gcc -o NthPrime main.c nth_prime.c
+
+./NthPrime $NUM
+
+# rm -rf "$SCRATCH"
