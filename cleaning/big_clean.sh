637c35754d603a20e53697a64c8c10084d12183a
Melissa Kamrowski <kamro010@opportunity.morris.umn.edu>
2019-09-03 22:24:43 -0500

    Pass most bats tests with completed script.
    One failing test appears to be due to technicality. Hope to fix before assignment deadline.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,10 +1,23 @@
-!#/usr/bin/env bash
+#!/usr/bin/env bash
 
 TAR_NAME=$1
 SCRATCH=$2
 
 tar zxf "$TAR_NAME" --directory "$SCRATCH"
 
-cd $SCRATCH 
+#gives all files containing text "DELETE ME!"
+DEL_FILES=$(grep -lr "DELETE ME!" "$SCRATCH") 
 
-grep 
+HERE=$(pwd)
+
+cd "$SCRATCH" || exit
+
+for file in $DEL_FILES
+do	
+	rm "$file"
+done
+
+cd "$HERE" || exit
+
+tar zcf "cleaned_$TAR_NAME" "$SCRATCH"
+
